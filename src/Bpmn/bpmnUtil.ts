0d48e1a4960a77752b0958b2b053f926d24a1dbe
Janneke Simmering <jsimmering@techfak.uni-bielefeld.de>
2022-08-19 13:36:35 +0200

    return processId of clicked or hovered SelectedActivity to enable opening of suprocess definition onCLick in frontend

--- src/Bpmn/bpmnUtil.ts
+++ src/Bpmn/bpmnUtil.ts
@@ -1,7 +1,5 @@
 import {ModdleElement} from "diagram-js/lib/model";
 
 export const getDocumentation = (businessObject: ModdleElement) => {
-    const documentation = businessObject?.documentation?.length ? businessObject.documentation[0].text : undefined;
-
-    return documentation;
+    return businessObject?.documentation?.length ? businessObject.documentation[0].text : undefined;
 };
